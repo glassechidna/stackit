3220c24f79ed8fd64a2ba9ad4a5a0c90d8cab016
Aidan Steele <aidan.steele@glassechidna.com.au>
2018-06-09 19:28:09 +1000

    CFN_TAG_<key>=val flow through as stack tags (closes #17)

--- stackit/up.go
+++ stackit/up.go
@@ -8,6 +8,8 @@ import (
 	"time"
 	"github.com/pkg/errors"
 	"github.com/davecgh/go-spew/spew"
+	"os"
+	"strings"
 )
 
 type StackitUpInput struct {
@@ -24,6 +26,16 @@ type StackitUpInput struct {
 
 func mapToTags(tagMap map[string]string) []*cloudformation.Tag {
 	tags := []*cloudformation.Tag{}
+
+	prefix := "CFN_TAG_"
+	for _, envvar := range os.Environ() {
+		if strings.HasPrefix(envvar, prefix) {
+			sansPrefix := envvar[len(prefix):]
+			keyval := strings.SplitN(sansPrefix, "=", 2)
+			tags = append(tags, &cloudformation.Tag{Key: &keyval[0], Value: &keyval[1]})
+
+		}
+	}
 
 	for key, val := range tagMap {
 		tags = append(tags, &cloudformation.Tag{Key: aws.String(key), Value: aws.String(val)})
